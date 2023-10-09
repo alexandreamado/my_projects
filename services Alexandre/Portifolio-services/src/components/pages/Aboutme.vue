8c8157bf9a8b781465265286999595db0acff51a
Alexandre Amado <alexandreamado71@gmail.com>
2023-10-09 14:08:31 +0200

    upload

--- services Alexandre/Portifolio-services/src/components/pages/Aboutme.vue
+++ services Alexandre/Portifolio-services/src/components/pages/Aboutme.vue
@@ -1,29 +1,42 @@
 <template>
   <div class="mt-16 flex flex-col gap-4 px-4 ml-8 border-l-[1px] border-white">
     <button
-      class="transition-all ease-in-out duration-700 flex items-center text-white hover:text-primary gap-2 hover:bg-header p-2 hover:w-full hover:rounded-md">
+      @click="activateMenuItem(0)"
+      :class="{ active: activeItem === 0 }"
+      class="transition-all ease-in-out duration-700 flex items-center text-white hover:text-primary gap-2 hover:bg-header p-2 hover:w-full hover:rounded-md"
+    >
       <i class="fa fa-user"></i>
       <p class="text-[16px] font-bold">Sobre mim</p>
     </button>
     <button
-      class="transition-all ease-in-out duration-700 flex items-center text-white hover:text-primary gap-2 hover:bg-header p-2 hover:w-full hover:rounded-md">
+      @click="activateMenuItem(1)"
+      :class="{ active: activeItem === 1 }"
+      class="transition-all ease-in-out duration-700 flex items-center text-white hover:text-primary gap-2 hover:bg-header p-2 hover:w-full hover:rounded-md"
+    >
       <i class="fas fa-desktop"></i>
       <p class="text-[16px] font-bold">Portfolio</p>
     </button>
 
     <button
-      class="transition-all ease-in-out duration-700 flex items-center text-white hover:text-primary gap-2 hover:bg-header p-2 hover:w-full hover:rounded-md">
+      @click="activateMenuItem(2)"
+      :class="{ active: activeItem === 2 }"
+      class="transition-all ease-in-out duration-700 flex items-center text-white hover:text-primary gap-2 hover:bg-header p-2 hover:w-full hover:rounded-md"
+    >
       <i class="fas fa-cogs"></i>
       <p class="text-[16px] font-bold">Serviços & Preços</p>
     </button>
 
     <button
+      @click="activateMenuItem(3)"
+      :class="{ active: activeItem === 3 }"
       class="transition-all ease-in-out duration-700 flex items-center text-white hover:text-primary gap-2 hover:bg-header p-2 hover:w-full hover:rounded-md"
     >
       <i class="fa fa-file"></i>
       <p class="text-[16px] font-bold">Resumo</p>
     </button>
     <button
+      @click="activateMenuItem(4)"
+      :class="{ active: activeItem === 4 }"
       class="transition-all ease-in-out duration-700 flex items-center text-white hover:text-primary gap-2 hover:bg-header p-2 hover:w-full hover:rounded-md"
     >
       <i class="fas fa-envelope"></i>
@@ -31,3 +44,26 @@
     </button>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeItem: null,
+    };
+  },
+  methods: {
+    activateMenuItem(index) {
+      this.activeItem = index;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.active {
+  background-color: #fafafa;
+  color: #000;
+  border-radius: 8px;
+}
+</style>
--- services Alexandre/Portifolio-services/src/components/pages/Oqueeufaco.vue
+++ services Alexandre/Portifolio-services/src/components/pages/Oqueeufaco.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="px-[12%] py-[3%]">
     <div class="flex flex-col gap-6">
-      <div class="flex items-center gap-6 mb-6 border-b-2 border-primary dark:border-white">
+      <div
+        class="flex items-center gap-6 mb-6 border-b-2 border-primary dark:border-white"
+      >
         <div class="flex items-center mb-4 gap-8">
           <Button class="font-bold dark:text-white"> Front-End </Button>
           <Button class="dark:text-white"> Técnico de Computadores </Button>
@@ -20,24 +22,23 @@
       </p>
     </div>
 
-    <div class="grid grid-cols-4">
+    <div class="grid grid-cols-3">
       <div class="flex flex-col w-[306px] mt-12 gap-4">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 448 512"
           class="w-8 h-8 dark:bg-white"
-          fill="#111821"
+          fill="#F7DF1E"
         >
           <path
             d="M0 32v448h448V32H0zm243.8 349.4c0 43.6-25.6 63.5-62.9 63.5-33.7 0-53.2-17.4-63.2-38.5l34.3-20.7c6.6 11.7 12.6 21.6 27.1 21.6 13.8 0 22.6-5.4 22.6-26.5V237.7h42.1v143.7zm99.6 63.5c-39.1 0-64.4-18.6-76.7-43l34.3-19.8c9 14.7 20.8 25.6 41.5 25.6 17.4 0 28.6-8.7 28.6-20.8 0-14.4-11.4-19.5-30.7-28l-10.5-4.5c-30.4-12.9-50.5-29.2-50.5-63.5 0-31.6 24.1-55.6 61.6-55.6 26.8 0 46 9.3 59.8 33.7L368 290c-7.2-12.9-15-18-27.1-18-12.3 0-20.1 7.8-20.1 18 0 12.6 7.8 17.7 25.9 25.6l10.5 4.5c35.8 15.3 55.9 31 55.9 66.2 0 37.8-29.8 58.6-69.7 58.6z"
           />
         </svg>
-        <h2 class="font-bold dark:text-white">Vanilla Javscript</h2>
+        <h2 class="font-bold dark:text-white">Javascript</h2>
         <p class="dark:text-white">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. At
-          blanditiis, tempora praesentium sapiente libero perferendis dolorem
-          ipsam enim ducimus vero placeat odit eum dolor amet omnis modi velit
-          rerum. Nulla?
+          Tenho habilidades sólidas em JavaScript, especialmente em
+          desenvolvimento front-end, abrangendo criação de aplicações web
+          interativas, manipulação do DOM, AJAX e consumo de APIs
         </p>
       </div>
 
@@ -54,16 +55,15 @@
           data-fa-i2svg=""
         >
           <path
-            fill="currentColor"
+            fill="#42B983"
             d="M356.9 64.3H280l-56 88.6-48-88.6H0L224 448 448 64.3h-91.1zm-301.2 32h53.8L224 294.5 338.4 96.3h53.8L224 384.5 55.7 96.3z"
           ></path>
         </svg>
         <h2 class="font-bold dark:text-white">Vuejs</h2>
         <p class="dark:text-white">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. At
-          blanditiis, tempora praesentium sapiente libero perferendis dolorem
-          ipsam enim ducimus vero placeat odit eum dolor amet omnis modi velit
-          rerum. Nulla?
+          Tenho sólidos conhecimentos em desenvolvimento de aplicações web com o
+          framework Vue.js. Uma das minhas áreas de especialização é a
+          implementação eficaz da comunicação entre componentes.
         </p>
       </div>
       <div class="flex flex-col w-[306px] mt-12 gap-4">
@@ -79,16 +79,14 @@
           data-fa-i2svg=""
         >
           <path
-            fill="currentColor"
+            fill="#3C823B"
             d="M224 508c-6.7 0-13.5-1.8-19.4-5.2l-61.7-36.5c-9.2-5.2-4.7-7-1.7-8 12.3-4.3 14.8-5.2 27.9-12.7 1.4-.8 3.2-.5 4.6.4l47.4 28.1c1.7 1 4.1 1 5.7 0l184.7-106.6c1.7-1 2.8-3 2.8-5V149.3c0-2.1-1.1-4-2.9-5.1L226.8 37.7c-1.7-1-4-1-5.7 0L36.6 144.3c-1.8 1-2.9 3-2.9 5.1v213.1c0 2 1.1 4 2.9 4.9l50.6 29.2c27.5 13.7 44.3-2.4 44.3-18.7V167.5c0-3 2.4-5.3 5.4-5.3h23.4c2.9 0 5.4 2.3 5.4 5.3V378c0 36.6-20 57.6-54.7 57.6-10.7 0-19.1 0-42.5-11.6l-48.4-27.9C8.1 389.2.7 376.3.7 362.4V149.3c0-13.8 7.4-26.8 19.4-33.7L204.6 9c11.7-6.6 27.2-6.6 38.8 0l184.7 106.7c12 6.9 19.4 19.8 19.4 33.7v213.1c0 13.8-7.4 26.7-19.4 33.7L243.4 502.8c-5.9 3.4-12.6 5.2-19.4 5.2zm149.1-210.1c0-39.9-27-50.5-83.7-58-57.4-7.6-63.2-11.5-63.2-24.9 0-11.1 4.9-25.9 47.4-25.9 37.9 0 51.9 8.2 57.7 33.8.5 2.4 2.7 4.2 5.2 4.2h24c1.5 0 2.9-.6 3.9-1.7s1.5-2.6 1.4-4.1c-3.7-44.1-33-64.6-92.2-64.6-52.7 0-84.1 22.2-84.1 59.5 0 40.4 31.3 51.6 81.8 56.6 60.5 5.9 65.2 14.8 65.2 26.7 0 20.6-16.6 29.4-55.5 29.4-48.9 0-59.6-12.3-63.2-36.6-.4-2.6-2.6-4.5-5.3-4.5h-23.9c-3 0-5.3 2.4-5.3 5.3 0 31.1 16.9 68.2 97.8 68.2 58.4-.1 92-23.2 92-63.4z"
           ></path>
         </svg>
         <h2 class="font-bold dark:text-white">Nodejs</h2>
         <p class="dark:text-white">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. At
-          blanditiis, tempora praesentium sapiente libero perferendis dolorem
-          ipsam enim ducimus vero placeat odit eum dolor amet omnis modi velit
-          rerum. Nulla?
+          Habilidades de desenvolvimento com a ferramente Nodejs no Back-End e
+          uso estabelecimento de conexao com banco de dados.
         </p>
       </div>
       <div class="flex flex-col w-[306px] mt-12 gap-4">
@@ -105,7 +103,7 @@
             data-fa-i2svg=""
           >
             <path
-              fill="currentColor"
+              fill="#DD4B25"
               d="M0 32l34.9 395.8L191.5 480l157.6-52.2L384 32H0zm308.2 127.9H124.4l4.1 49.4h175.6l-13.6 148.4-97.9 27v.3h-1.1l-98.7-27.3-6-75.8h47.7L138 320l53.5 14.5 53.7-14.5 6-62.2H84.3L71.5 112.2h241.1l-4.4 47.7z"
             ></path>
           </svg>
@@ -121,7 +119,7 @@
             data-fa-i2svg=""
           >
             <path
-              fill="currentColor"
+              fill="#254BDD"
               d="M0 32l34.9 395.8L192 480l157.1-52.2L384 32H0zm313.1 80l-4.8 47.3L193 208.6l-.3.1h111.5l-12.8 146.6-98.2 28.7-98.8-29.2-6.4-73.9h48.9l3.2 38.3 52.6 13.3 54.7-15.4 3.7-61.6-166.3-.5v-.1l-.2.1-3.6-46.3L193.1 162l6.5-2.7H76.7L70.9 112h242.2z"
             ></path>
           </svg>
@@ -129,45 +127,65 @@
 
         <h2 class="font-bold dark:text-white">Html 5 & CSS 3</h2>
         <p class="dark:text-white">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. At
-          blanditiis, tempora praesentium sapiente libero perferendis dolorem
-          ipsam enim ducimus vero placeat odit eum dolor amet omnis modi velit
-          rerum. Nulla?
+          conhecimentos solidos de Html e css para estilizacao de paginas
+          webs,responsividade e animacoes da pagina web.
         </p>
       </div>
       <div class="flex flex-col w-[306px] mt-12 gap-4">
-        <svg
-          class="w-2/3 dark:text-white"
-          aria-hidden="true"
-          viewBox="0 0 160 24"
-          fill="none"
-        >
-          <path
-            d="M18.724 1.714c-4.538 0-7.376 2.286-8.51 6.857 1.702-2.285 3.687-3.143 5.957-2.57 1.296.325 2.22 1.271 3.245 2.318 1.668 1.706 3.6 3.681 7.819 3.681 4.539 0 7.376-2.286 8.51-6.857-1.701 2.286-3.687 3.143-5.957 2.571-1.294-.325-2.22-1.272-3.245-2.32-1.668-1.705-3.6-3.68-7.819-3.68zM10.214 12c-4.539 0-7.376 2.286-8.51 6.857 1.701-2.286 3.687-3.143 5.957-2.571 1.294.325 2.22 1.272 3.245 2.32 1.668 1.705 3.6 3.68 7.818 3.68 4.54 0 7.377-2.286 8.511-6.857-1.702 2.286-3.688 3.143-5.957 2.571-1.295-.326-2.22-1.272-3.245-2.32-1.669-1.705-3.6-3.68-7.82-3.68z"
-            class="fill-[#000] dark:fill-[#fff]"
-          ></path>
-         
-        </svg>
-        <h2 class="font-bold dark:text-white">Tailwind CSS</h2>
+        <div class="flex gap-6">
+          <svg
+            class="w-8 h-8"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 54 33"
+          >
+            <g clip-path="url(#prefix__clip0)">
+              <path
+                fill="#38bdf8"
+                fill-rule="evenodd"
+                d="M27 0c-7.2 0-11.7 3.6-13.5 10.8 2.7-3.6 5.85-4.95 9.45-4.05 2.054.513 3.522 2.004 5.147 3.653C30.744 13.09 33.808 16.2 40.5 16.2c7.2 0 11.7-3.6 13.5-10.8-2.7 3.6-5.85 4.95-9.45 4.05-2.054-.513-3.522-2.004-5.147-3.653C36.756 3.11 33.692 0 27 0zM13.5 16.2C6.3 16.2 1.8 19.8 0 27c2.7-3.6 5.85-4.95 9.45-4.05 2.054.514 3.522 2.004 5.147 3.653C17.244 29.29 20.308 32.4 27 32.4c7.2 0 11.7-3.6 13.5-10.8-2.7 3.6-5.85 4.95-9.45 4.05-2.054-.513-3.522-2.004-5.147-3.653C23.256 19.31 20.192 16.2 13.5 16.2z"
+                clip-rule="evenodd"
+              />
+            </g>
+            <defs>
+              <clipPath id="prefix__clip0">
+                <path fill="#fff" d="M0 0h54v32.4H0z" />
+              </clipPath>
+            </defs>
+          </svg>
+          <svg
+            fill="#8512FB"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Bootstrap icon</title>
+            <path
+              d="M20 0H4C1.793.006.006 1.793 0 4v16c0 2.2 1.8 4 4 4h16c2.2 0 4-1.8 4-4V4c0-2.2-1.8-4-4-4zm-2.187 16.855c-.2.482-.517.907-.923 1.234-.42.34-.952.62-1.607.82-.654.203-1.432.305-2.333.305H6.518v-14h6.802c1.258 0 2.266.283 3.02.86.76.58 1.138 1.444 1.138 2.61 0 .705-.172 1.31-.518 1.81-.344.497-.84.886-1.48 1.156v.046c.854.18 1.515.585 1.95 1.215s.658 1.426.658 2.387c0 .538-.104 1.05-.3 1.528l.025.027zm-2.776-3.45c-.41-.375-.986-.558-1.73-.558H8.985v4.368h4.334c.74 0 1.32-.192 1.73-.58.41-.385.62-.934.62-1.64-.007-.69-.21-1.224-.62-1.59h-.017zm-.6-2.823c.396-.336.59-.817.59-1.444 0-.704-.175-1.204-.53-1.49-.352-.285-.86-.433-1.528-.433h-4v3.863h4c.583 0 1.08-.17 1.464-.496z"
+            />
+          </svg>
+        </div>
+        <h2 class="font-bold dark:text-white">Tailwind CSS & Bootstrap</h2>
         <p class="dark:text-white">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. At
-          blanditiis, tempora praesentium sapiente libero perferendis dolorem
-          ipsam enim ducimus vero placeat odit eum dolor amet omnis modi velit
-          rerum. Nulla?
+          Desenvolvimento de layout responsivo e adaptacoes para qualquer
+          dispositivo web.
         </p>
       </div>
       <div class="flex flex-col w-[306px] mt-12 gap-4">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 448 512"
-          class="w-8 h-8 dark:bg-white"
-          fill="#111821"
+          viewBox="0 0 128 128"
+          id="mysql"
+          class="w-6 h-6"
         >
           <path
-            d="M0 32v448h448V32H0zm243.8 349.4c0 43.6-25.6 63.5-62.9 63.5-33.7 0-53.2-17.4-63.2-38.5l34.3-20.7c6.6 11.7 12.6 21.6 27.1 21.6 13.8 0 22.6-5.4 22.6-26.5V237.7h42.1v143.7zm99.6 63.5c-39.1 0-64.4-18.6-76.7-43l34.3-19.8c9 14.7 20.8 25.6 41.5 25.6 17.4 0 28.6-8.7 28.6-20.8 0-14.4-11.4-19.5-30.7-28l-10.5-4.5c-30.4-12.9-50.5-29.2-50.5-63.5 0-31.6 24.1-55.6 61.6-55.6 26.8 0 46 9.3 59.8 33.7L368 290c-7.2-12.9-15-18-27.1-18-12.3 0-20.1 7.8-20.1 18 0 12.6 7.8 17.7 25.9 25.6l10.5 4.5c35.8 15.3 55.9 31 55.9 66.2 0 37.8-29.8 58.6-69.7 58.6z"
-          />
+            fill="#00618A"
+            d="M116.948 97.807c-6.863-.187-12.104.452-16.585 2.341-1.273.537-3.305.552-3.513 2.147.7.733.809 1.829 1.365 2.731 1.07 1.73 2.876 4.052 4.488 5.268 1.762 1.33 3.577 2.751 5.465 3.902 3.358 2.047 7.107 3.217 10.34 5.268 1.906 1.21 3.799 2.733 5.658 4.097.92.675 1.537 1.724 2.732 2.147v-.194c-.628-.8-.79-1.898-1.366-2.733l-2.537-2.537c-2.48-3.292-5.629-6.184-8.976-8.585-2.669-1.916-8.642-4.504-9.755-7.609l-.195-.195c1.892-.214 4.107-.898 5.854-1.367 2.934-.786 5.556-.583 8.585-1.365l4.097-1.171v-.78c-1.531-1.571-2.623-3.651-4.292-5.073-4.37-3.72-9.138-7.437-14.048-10.537-2.724-1.718-6.089-2.835-8.976-4.292-.971-.491-2.677-.746-3.318-1.562-1.517-1.932-2.342-4.382-3.511-6.633-2.449-4.717-4.854-9.868-7.024-14.831-1.48-3.384-2.447-6.72-4.293-9.756-8.86-14.567-18.396-23.358-33.169-32-3.144-1.838-6.929-2.563-10.929-3.513-2.145-.129-4.292-.26-6.438-.391-1.311-.546-2.673-2.149-3.902-2.927-4.894-3.092-17.448-9.817-21.072-.975-2.289 5.581 3.421 11.025 5.462 13.854 1.434 1.982 3.269 4.207 4.293 6.438.674 1.467.79 2.938 1.367 4.489 1.417 3.822 2.652 7.98 4.487 11.511.927 1.788 1.949 3.67 3.122 5.268.718.981 1.951 1.413 2.145 2.927-1.204 1.686-1.273 4.304-1.95 6.44-3.05 9.615-1.899 21.567 2.537 28.683 1.36 2.186 4.567 6.871 8.975 5.073 3.856-1.57 2.995-6.438 4.098-10.732.249-.973.096-1.689.585-2.341v.195l3.513 7.024c2.6 4.187 7.212 8.562 11.122 11.514 2.027 1.531 3.623 4.177 6.244 5.073v-.196h-.195c-.508-.791-1.303-1.119-1.951-1.755-1.527-1.497-3.225-3.358-4.487-5.073-3.556-4.827-6.698-10.11-9.561-15.609-1.368-2.627-2.557-5.523-3.709-8.196-.444-1.03-.438-2.589-1.364-3.122-1.263 1.958-3.122 3.542-4.098 5.854-1.561 3.696-1.762 8.204-2.341 12.878-.342.122-.19.038-.391.194-2.718-.655-3.672-3.452-4.683-5.853-2.554-6.07-3.029-15.842-.781-22.829.582-1.809 3.21-7.501 2.146-9.172-.508-1.666-2.184-2.63-3.121-3.903-1.161-1.574-2.319-3.646-3.124-5.464-2.09-4.731-3.066-10.044-5.267-14.828-1.053-2.287-2.832-4.602-4.293-6.634-1.617-2.253-3.429-3.912-4.683-6.635-.446-.968-1.051-2.518-.391-3.513.21-.671.508-.951 1.171-1.17 1.132-.873 4.284.29 5.462.779 3.129 1.3 5.741 2.538 8.392 4.294 1.271.844 2.559 2.475 4.097 2.927h1.756c2.747.631 5.824.195 8.391.975 4.536 1.378 8.601 3.523 12.292 5.854 11.246 7.102 20.442 17.21 26.732 29.269 1.012 1.942 1.45 3.794 2.341 5.854 1.798 4.153 4.063 8.426 5.852 12.488 1.786 4.052 3.526 8.141 6.05 11.513 1.327 1.772 6.451 2.723 8.781 3.708 1.632.689 4.307 1.409 5.854 2.34 2.953 1.782 5.815 3.903 8.586 5.855 1.383.975 5.64 3.116 5.852 4.879zM29.729 23.466c-1.431-.027-2.443.156-3.513.389v.195h.195c.683 1.402 1.888 2.306 2.731 3.513.65 1.367 1.301 2.732 1.952 4.097l.194-.193c1.209-.853 1.762-2.214 1.755-4.294-.484-.509-.555-1.147-.975-1.755-.556-.811-1.635-1.272-2.339-1.952z"
+          ></path>
         </svg>
-        <h2 class="font-bold dark:text-white">Vanilla Javscript</h2>
+        <h2 class="font-bold dark:text-white">Mysql</h2>
         <p class="dark:text-white">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. At
           blanditiis, tempora praesentium sapiente libero perferendis dolorem
